a1191eed4425f5aa63524bfe69c3460339e77460
lior2k <92747945+lior2k@users.noreply.github.com>
2022-01-01 03:29:34 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include "graph.h"
+#define backslashn 10
+
+void print_graph(pnode *head) {
+    pnode temp = *head;
+    while (temp != NULL) {
+        printf("node id: %d, node dist: %d ", temp -> id, temp -> dist);
+        pedge edge = temp -> edges;
+        while (edge != NULL) {
+            printf("dest: %d, ", edge -> endpoint);
+            printf("w: %d ", edge -> weight);
+            edge = edge -> next;
+        }
+        temp = temp -> next;
+        printf("\n");
+    }
+}
+
+int main() {
+    pnode temp_p = NULL;
+    pnode *head = &temp_p;
+    char x = 0;
+    while (x != backslashn) {
+        x = getc(stdin);
+        if (x == 'A') {
+
+            build_graph_cmd(head);
+
+        } else if (x == 'B') {
+
+            insert_new_node_cmd(head);
+
+        } else if (x == 'D') {
+
+            delete_node_cmd(head);
+
+        } else if (x == 'S') {
+
+            shortsPath_cmd(head);
+
+        } else if (x == 'T') {
+            
+            TSP_cmd(head);
+
+        } else if (x == 'P') {
+
+            print_graph(head);
+
+        }
+    }
+}
